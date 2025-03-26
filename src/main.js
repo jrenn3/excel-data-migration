5baf7d3d7b81c87176a5e83875dd30c47277427a
jrenn3 <jrenn3@gmail.com>
2025-03-25 20:41:00 -0500

    code cleanup

--- src/main.js
+++ src/main.js
@@ -1,9 +1,1 @@
 import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <div class="card">
-      <button id="counter" type="button">Upload</button>
-    </div>
-  </div>
-`
