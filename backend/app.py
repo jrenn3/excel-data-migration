bb8803fe88ffbbfe948d6b33cdb87fe381a08a93
jrenn3 <jrenn3@gmail.com>
2025-03-29 08:12:17 -0500

    configure for deployment

--- backend/app.py
+++ backend/app.py
@@ -63,4 +63,5 @@ def upload():
         os.unlink(uploaded_path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # fallback to 5000 for local dev
+    app.run(host='0.0.0.0', port=port)
